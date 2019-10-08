b5324512848ece8bb89e4442f196f68ba2abcc9b
Adeeb <softwarestudent199@protonmail.com>
2019-10-08 23:02:13 +0500

    portfolio project

---
+++ src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Adeeb Ibrhaim",
+  role: "full stack developer",
+  Instagramid: "https://www.instagram.com/adeebibrhaim.js/",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "Performance-driven and motivated Full Stack Developer with 3+ years of extensive experience in participating in all phases of software development. Adept at managing and supervising other employees and determined to achieve extraordinary results. Possess a strong attention to detail, well-developed time management skills, and ability to complete all projects within schedule and in a timely manner.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/adeeb-ibrhaim-6a4611184/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/Adeeb1546",
+      className: "fa fa-github"
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/Aadieeeibrhaim",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Islamabad",
+  phone: "+923420501829",
+  education: [
+    {
+      UniversityName: "B.A.S.C.O.M.S",
+      specialization: "Matric",
+      MonthOfPassing: "jun",
+      YearOfPassing: "2014"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "Punjab college kotli azad kashmir",
+      specialization: "FSC Premedical",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2016"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "Capital university of science & Technology islamabad",
+      specialization: "Software engineering",
+      MonthOfPassing: "sep",
+      YearOfPassing: "2017"
+      // Achievements: "Some Achievements"
+    }
+  ],
+  work: [
+    {
+      // CompanyName: "Freelancing",
+      specialization: "Freelancing",
+      MonthOfLeaving: "Mar",
+      YearOfLeaving: "2017"
+      // Achievements: "Some Achievements"
+    },
+    {
+      // CompanyName: "Freelancing",
+      specialization: "Upwork",
+      MonthOfLeaving: "Sep",
+      YearOfLeaving: "2018"
+      // Achievements: "Some Achievements"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "Bootstrap"
+    },
+    {
+      skillname: "Reactjs"
+    },
+    {
+      skillname: "Nodejs"
+    },
+    {
+      skillname: "sass"
+    },
+    {
+      skillname: "Java"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Categories",
+      description: "https://crwn-live123.herokuapp.com/",
+      imgurl: "images/1.jpg"
+    },
+    {
+      name: "Items",
+      description: "https://crwn-live123.herokuapp.com/shop",
+      imgurl: "images/2.jpg"
+    },
+    {
+      name: "SignIn SignUp ",
+      description: "https://crwn-live123.herokuapp.com/signin",
+      imgurl: "images/3.jpg"
+    },
+    {
+      name: "CheckOut",
+      description: "https://crwn-live123.herokuapp.com/checkout",
+      imgurl: "images/4.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "",
+      name: ""
+    }
+  ]
+};
+
+export default resumeData;
